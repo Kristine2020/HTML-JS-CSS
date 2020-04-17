94fe7a7f47e2a10f9f37940b4c9b3dfed805ae8b
Kristine2020 <62056033+Kristine2020@users.noreply.github.com>
2020-04-18 00:19:32 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+function calculateGrade() {
+
+  var participationGrade = parseFloat(document.getElementById("participationGrade").innerHTML)
+  var quizGrade = parseFloat(document.getElementById("quizGrade").innerHTML)
+  var homeworkGrade = parseFloat(document.getElementById("homeworkGrade").innerHTML)
+  var project1Grade = parseFloat(document.getElementById("project1Grade").innerHTML) 
+  var project2Grade = parseFloat(document.getElementById("project2Grade").innerHTML)
+
+
+  var participationPercent = parseInt(document.getElementById("participationPercent").innerHTML)
+  var quizPercent = parseInt(document.getElementById("quizPercent").innerHTML)
+  var homeworkPercent = parseInt(document.getElementById("homeworkPercent").innerHTML)
+  var project1Percent = parseInt(document.getElementById("project1Percent").innerHTML)
+  var project2Percent = parseInt(document.getElementById("project2Percent").innerHTML)
+
+
+  var participation = participationGrade * participationPercent / 100
+  var quiz = quizGrade * quizPercent / 100
+  var homework = homeworkGrade * homeworkPercent / 100
+  var project1 = project1Grade * project1Percent / 100
+  var project2 = project2Grade * project2Percent / 100
+
+  var Final_Grade = participation + quiz + homework + project1 + project2
+
+  console.log(Final_Grade) 
+  var display=document.getElementById("calculateGrade");
+    display.innerHTML=Final_Grade;
+  
+}
